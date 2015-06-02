c45afe09591ba0d701590634c387ce25dc91a0b7
Kotaro Hara <koe.bluebear@gmail.com>
2015-06-01 22:14:36 -0400

    Added tests to haversine formula in LatLng

--- ToSidewalk/__init__.py
+++ ToSidewalk/__init__.py
@@ -1,6 +1,4 @@
 """ToSidewalk generates potential sidewalk network from the exsisting OpenStreetMap street network data.
 """
 
-from . import ToSidewalk
-
 __version__ = '0.0.1'
--- ToSidewalk/test/test_latlng.py
+++ ToSidewalk/test/test_latlng.py
@@ -1,5 +1,30 @@
-def f():
-    return 3
+import unittest
+import math
+from ToSidewalk.latlng import haversine
 
-def test_function():
-    assert f() == 4
+class TestLatLngMethods(unittest.TestCase):
+    def test_haversine(self):
+        """
+        Haversine ground truth is from:
+        http://andrew.hedges.name/experiments/haversine/
+        """
+        latlng1 = [38.898556, -77.037852]
+        latlng2 = [38.897147, -77.043934]
+        latlng1 = [math.radians(x) for x in latlng1]
+        latlng2 = [math.radians(x) for x in latlng2]
+
+        distance = 0.549
+        error = abs(distance - haversine(latlng1[1], latlng1[0], latlng2[1], latlng2[0]))
+        self.assertTrue(error < 0.001)
+
+        latlng1 = [38.900665, -76.983008]
+        latlng2 = [38.9007234, -76.98197]
+        latlng1 = [math.radians(x) for x in latlng1]
+        latlng2 = [math.radians(x) for x in latlng2]
+        distance = 0.09
+
+        error = abs(distance - haversine(latlng1[1], latlng1[0], latlng2[1], latlng2[0]))
+        self.assertTrue(error < 0.001)
+
+if __name__ == '__main__':
+    unittest.main()
