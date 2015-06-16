a910f26aa6a38149216868790c76c544a18c39b9
Kotaro Hara <koe.bluebear@gmail.com>
2015-06-16 15:31:05 -0400

    minor fix

--- ToSidewalk/ToSidewalk.py
+++ ToSidewalk/ToSidewalk.py
@@ -262,8 +262,8 @@ if __name__ == "__main__":
     # filename = "../resources/SimpleWay_01.osm"
     # filename = "../resources/Simple4WayIntersection_01.osm"
     # filename = "../resources/SmallMap_01.osm"
-    # filename = "../resources/ParallelLanes_01.osm"
-    filename = "../resources/SmallMap_04.osm"
+    filename = "../resources/ParallelLanes_01.osm"
+    # filename = "../resources/SmallMap_02.osm"
     # filename = "../resources/SegmentedStreet_01.osm"
     #filename = "../resources/ParallelLanes_03.osm"
 
