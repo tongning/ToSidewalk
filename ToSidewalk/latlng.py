fab5f38d390c105bec2a45d71006959b6efb4b87
Kotaro Hara <koe.bluebear@gmail.com>
2015-07-02 21:23:02 -0400

    Done merging nearby nodes

--- ToSidewalk/latlng.py
+++ ToSidewalk/latlng.py
@@ -37,11 +37,12 @@ def haversine(lon1, lat1, lon2, lat2):
     :param lat1: Latitude of the first point
     :param lon2: Longitude of the second point
     :param lat2: Latitude of the second point
+    :return: A distance in meters
     """
     # haversine formula
     dlon = lon2 - lon1
     dlat = lat2 - lat1
     a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
     c = 2 * asin(sqrt(a))
-    r = 6371  # Radius of earth in kilometers. Use 3956 for miles
+    r = 6371000  # Radius of earth in meters
     return c * r
